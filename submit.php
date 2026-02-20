f20b8ff92e0a7461722329575ba822979669c95c
Janavalsan-cms <janavalsan@mindstory.in>
2026-02-20 13:28:36 +0530

    error fix

--- submit.php
+++ submit.php
@@ -198,7 +198,7 @@ else {
 // ======================
 $headers = "MIME-Version: 1.0\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8\r\n";
-$headers .= "From: Website <noreply@janavalsans@gmail.com>\r\n";
+$headers .= "From: Viral Cat <noreply@viralcatmeow.com>\r\n";
 $headers .= "Reply-To: $email\r\n";
 
 // ======================
